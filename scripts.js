d9d730ded2fdd4a356c911d6e92f6a8b03168c87
Nate Mills <natemills018@gmail.com>
2023-12-03 20:59:57 -0500

    Adding some CSS, starting to create class functioning tomorrow

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let canvas = document.getElementById('canvas');
+
+
+
+class Shapes {
+    constructor() {
+        this.value = null
+        this.div = document.createElement('div');
+        this.div.classList.add('Shapes');
+    }
+
+    
+}
